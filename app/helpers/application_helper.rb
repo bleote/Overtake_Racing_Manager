fec16bb1ca69ed8286b999456ec21d98118ec838
Bruno Leote <b.leote@gmail.com>
2023-04-21 12:52:56 +0200

    Application Helper updated so the navbar brings the correct user image between default or uploaded

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def check_photo(user)
+    if user.photo_attachment.nil?
+      image_tag "photo_base.png", class: "avatar dropdown-toggle", id: "navbarDropdown", data: { bs_toggle: "dropdown" }, 'aria-haspopup': true, 'aria-expanded': false
+    else
+      cl_image_tag user.photo.key, class: "avatar dropdown-toggle", id: "navbarDropdown", data: { bs_toggle: "dropdown" }, 'aria-haspopup': true, 'aria-expanded': false
+    end
+  end
 end
